614bd9167e24d45f9f3c9433d9590433f827f7ca
Yaman Kalaji <syk.yaman@gmail.com>
2023-11-17 20:01:22 +0000

    Add SD card test

--- Code/AirPressure/AirPressure.ino
+++ Code/AirPressure/AirPressure.ino
@@ -2,6 +2,11 @@
 
 #include <Wire.h>
 #include "Adafruit_MPRLS.h"
+#include <SPI.h>
+#include <SD.h>
+
+int count = 0;
+const int chipSelect = 4;
 
 // You dont *need* a reset and EOC pin for most uses, so we set to -1 and don't connect
 #define RESET_PIN  -1  // set to any GPIO pin # to hard-reset on begin()
@@ -10,6 +15,17 @@ Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
 
 void setup() {
   Serial.begin(115200);
+  
+   Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+  
   Serial.println("MPRLS Simple Test");
   if (! mpr.begin()) {
     Serial.println("Failed to communicate with MPRLS sensor, check wiring?");
@@ -22,9 +38,32 @@ void setup() {
 
 
 void loop() {
+  count++;
+  if(count <20){
+   String dataString = "";
+   
   float pressure_hPa = mpr.readPressure();
   //Serial.print("Pressure (hPa): "); 
+  dataString += String(pressure_hPa);
   Serial.println(pressure_hPa);
+
+ // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+  
+  }
   //Serial.print("Pressure (PSI): "); Serial.println(pressure_hPa / 68.947572932);
   delay(1000);
 }
