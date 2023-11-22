7cb096b33ec63fa7a2bfaa8aebdbc11ec236dfbe
Yaman Kalaji <syk.yaman@gmail.com>
2023-11-22 23:33:05 +0000

    Fix Python datetime setter code

--- Code/RTC-setter/TimeSetter.py
+++ Code/RTC-setter/TimeSetter.py
@@ -1,12 +1,14 @@
 import serial
 import time, datetime, os
 
-serialPort = serial.Serial(port=serialPortNo, baudrate=921600, bytesize=8, timeout=10, stopbits=serial.STOPBITS_ONE)
+serialPortNo = "COM49"
 
-now = datetime.now()
+serialPort = serial.Serial(port=serialPortNo, baudrate=115200, bytesize=8, timeout=10, stopbits=serial.STOPBITS_ONE)
+
+now = datetime.datetime.now()
 #example: 50,37,20,17,11,23
 
 dt_string = now.strftime("%S,%M,%H,%d,%m,%y")
 print(dt_string)
 
-serialPort.write(dt_string) 
+serialPort.write(dt_string.encode()) 
